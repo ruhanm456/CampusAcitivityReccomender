e2b99841850f097fe9c12f80b6d04b2ea048a103
salmancollege <ruhanm@uni.edu>
2026-01-03 10:25:10 -0500

    push

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_verified = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
