0689d1d7b812b48a63626458285ecdf5f633a05d
ruhanm456 <ruhanm@uni.edu>
2026-04-01 11:02:11 -0400

    user login progress

--- app/api/main.py
+++ app/api/main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
-from db.models import Session, User
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    # set up DB
-    return {"message": "Server started"}
+# User data model (Input format)
+class LoginRequest(BaseModel):
+    email: str
+    password: str
 
-@app.post("/clubs/create")
-def create_club(name: str, description: str | None = None):
-  pass
+@app.post("/login")
+async def login(data: LoginRequest):
+    # Actual database check will happen here. Currently providing an example:
+    if data.email == "test@example.com" and data.password == "password123":
+        return {
+            "status": "success",
+            "token": "fake-jwt-token-123", # This is the digital pass (token)
+            "user": {"name": "Ruhan", "year": "Freshman"}
+        }
+    else:
+        # Raise error if password or email is incorrect
+        raise HTTPException(status_code=401, detail="Incorrect email or password!")
